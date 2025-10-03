57fadcc74302631aade8bdfd30327d3ea933ca62
dilanputraherlambang <dilanputraherlambang@gmail.com>
2025-10-03 21:32:19 +0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const tombolSapa = document.getElementById('sapaButton'); // Corrected spelling and capitalization
-tombolSapa.addEventListener('click', function() {
-  alert('Halo! Terima kasih sudah berkunjung!');
-});
